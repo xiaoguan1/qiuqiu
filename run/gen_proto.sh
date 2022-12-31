c1a28a1d6b8cb55e3d8c4231384f370f6de74aac
xiaoguan1 <1326317931@qq.com>
2023-01-01 01:14:11 +0800

    协议文件

--- run/gen_proto.sh
+++ run/gen_proto.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-# getDir(){
-# 	dir=`echo $0 | grep "^/"`
-# 	if test "${dir}"; then	# test 变量。 dir 为空字符串("") 则为false，否则为true
-# 		dirname $0
-# 	else
-# 		dirname `pwd`/$0
-# 	fi
-# }
-
-# RUNUSER=`whoami` # 当前用户
-# nogame=$1
-# if [ "$RUNUSER" != "game" -a "X$nogame" = "X" ]; then
-# 	echo "$0 must be run by game"
-# 	exit
-# fi
-
-# RUNDIR=`getDir`	# 当前路径
-
 lua ./gen_proto/gen_proto.lua
--- run/gen_proto/gen_proto.lua
+++ run/gen_proto/gen_proto.lua
@@ -1,8 +1,8 @@
--------- 必须具备这些脚本文件 --------
+-------- 必须具备这些自定义脚本文件 --------
 dofile "../lualib/table.lua"
 dofile "../lualib/string.lua"
 local filelib = dofile "../lualib/filelib.lua"
--------- 必须具备这些脚本文件 --------
+-------- 必须具备这些自定义脚本文件 --------
 
 -- 计算绝对路径
 local nowPath = filelib.getNowPath()
@@ -12,14 +12,20 @@ local pbPath = nowPath .. "/../protobuf/pb/"
 local protofiles = filelib.getFileList(protoPath)
 if not protofiles or type(protofiles) ~= "table" or table.empty(protofiles) then
 	error("not find protofiles")
-	return
 end
 
+-- 目前还有一些问题，编译协议时是无序如果后面有编译顺序的需求需要加代码!!!
 for _, filename in pairs(protofiles) do
 	local fname = string.split(filename, "%.", 1)
 	if not fname then break end
 
-	local newfileProtoPath = protoPath .. filename
+	local fileProtoPath = protoPath .. filename
 	local newfilePbPath = pbPath .. fname .. ".pb"
-	os.execute("protoc --proto_path=" .. protoPath .. " -o " .. newfilePbPath .. " " .. newfileProtoPath)
+
+	local isOk = os.execute("protoc --proto_path=" .. protoPath .. " -o " .. newfilePbPath .. " " .. fileProtoPath)	
+	if not isOk then
+		error(string.format("protobuf compile error, file:[%s]", filename))
+	end
 end
+
+print("protobuf compile succeed")
