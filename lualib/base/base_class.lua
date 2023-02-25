b3ec1aec8d87017c61437616ab15d06114467494
guanguowei <1326317931@qq.com>
2023-02-25 18:34:52 +0800

    云风的类对象案例

--- lualib/base/base_class.lua
+++ lualib/base/base_class.lua
@@ -5,7 +5,7 @@ local _class = {}
 function class(super)
 	local class_type = {}
 	class_type.ctor = false
-	class_type.super = super
+	class_type.super = super    -- 继承
 	class_type.new = function(...) 
 			local obj = {}
 			do
@@ -44,7 +44,7 @@ end
 
 ----- 使用
 base_type = class()		-- 定义一个基类 base_type
- 
+
 function base_type:ctor(x)	-- 定义 base_type 的构造函数
 	print("base_type ctor")
 	self.xc=cx
@@ -59,18 +59,22 @@ function base_type:hello()	-- 定义另一个成员函数 base_type:hello
 end
 
 
------ 继承
+-- ----- 继承
 test = class(base_type)	-- 定义一个类 test 继承于 base_type
 
-function test:ctor()	-- 定义 test 的构造函数
-	print("test ctor")
+for k, v in pairs(_class) do
+    print(k, v)
 end
 
-function test:hello()	-- 重载 base_type:hello 为 test:hello
-	print("hello test")
-end
+-- function test:ctor()	-- 定义 test 的构造函数
+-- 	print("test ctor")
+-- end
 
-a = test.new(1)	-- 输出两行，base_type ctor 和 test ctor 。这个对象被正确的构造了。
-a:print_x()	-- 输出 1 ，这个是基类 base_type 中的成员函数。
-a:hello()	-- 输出 hello test ，这个函数被重载了。
+-- function test:hello()	-- 重载 base_type:hello 为 test:hello
+-- 	print("hello test")
+-- end
+
+-- a = test.new(1)	-- 输出两行，base_type ctor 和 test ctor 。这个对象被正确的构造了。
+-- a:print_x()	-- 输出 1 ，这个是基类 base_type 中的成员函数。
+-- a:hello()	-- 输出 hello test ，这个函数被重载了。
 
