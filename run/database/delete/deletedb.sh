40d8d2995e0751ad4bad86006d1c584cc8571e2a
guanguowei <1326317931@qq.com>
2024-05-08 22:31:28 +0800

    删除数据库脚本

--- run/database/delete/deletedb.sh
+++ run/database/delete/deletedb.sh
@@ -8,3 +8,38 @@ if ["$isTrue"x == "yes"x]; then
     echo "input error!!!"
     exit
 fi
+
+mysql -hlocalhost -uroot -proot $dbname << EOF 2 >>/dev/null
+delete from globalusr;
+EOF
+[ $? eq 0 ] && echo "delete table globalusr ok" || echo "delete table globalusr fail";
+
+mysql -hlocalhost -uroot -proot $dbname << EOF 2 >>/dev/null
+delete from list;
+EOF
+[ $? eq 0 ] && echo "delete table list ok" || echo "delete table list fail";
+
+mysql -hlocalhost -uroot -proot $dbname << EOF 2 >>/dev/null
+delete from role_data;
+EOF
+[ $? eq 0 ] && echo "delete table role_data ok" || echo "delete table role_data fail";
+
+mysql -hlocalhost -uroot -proot $dbname << EOF 2 >>/dev/null
+delete from role;
+EOF
+[ $? eq 0 ] && echo "delete table role ok" || echo "delete table role fail";
+
+mysql -hlocalhost -uroot -proot $dbname << EOF 2 >>/dev/null
+delete from module;
+EOF
+[ $? eq 0 ] && echo "delete table module ok" || echo "delete table module fail";
+
+mysql -hlocalhost -uroot -proot $dbname << EOF 2 >>/dev/null
+delete from setting;
+EOF
+[ $? eq 0 ] && echo "delete table setting ok" || echo "delete table setting fail";
+
+mysql -hlocalhost -uroot -proot $dbname << EOF 2 >>/dev/null
+delete from battlerecord;
+EOF
+[ $? eq 0 ] && echo "delete table battlerecord ok" || echo "delete table battlerecord fail";
