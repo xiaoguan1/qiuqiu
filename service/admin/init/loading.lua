6422640a414a05861b46e5d12658b215af8ef03c
xiaoguan1 <1326317931@qq.com>
2024-07-03 00:25:08 +0800

    加载数据文件

--- service/admin/init/loading.lua
+++ service/admin/init/loading.lua
@@ -1,3 +1,4 @@
 PROXYSVR = Import("lualib/base/proxysvr.lua")
 MANAGE_MGR = Import("service/admin/module/manage/manage_mgr.lua")
 CALLOUT = Import("lualib/call_out.lua")
+MODULE_DB = Import("lualib/module_db.lua")
